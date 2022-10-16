1273967723d34e0b18263ef5acae02debfb91380
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2022-10-16 21:57:06 +0200

    commit

---
+++ src/homework/elevenoctober/ElevenOctoberSecondLevel.java
@@ -0,0 +1,20 @@
+package homework.elevenoctober;
+
+import java.util.Scanner;
+
+public class ElevenOctoberSecondLevel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Джон едет на поезде к Мэри. Шерлок Холмс решил вычислить, где сейчас находится Джон:
+        // ближе к нему или к Мэри.
+        //Шерлок знает, с какой скоростью едет поезд и сколько часов он уже едет.
+        // Также он знает, сколько километров длится полная дорога от него к  Мэри.
+        //В программу вводится 3 числа: время в пути, скорость поезда, длина полного пути.
+        //Вывести на экран имя персонажа, к которому сейчас ближе Джон
+    int time = sc.nextInt();
+    int speed = sc.nextInt();
+    int length = sc.nextInt();
+
+
+    }
+}
---
+++ src/homework/thirteenoctober/ThirteenOctoberFirstLevel.java
@@ -0,0 +1,16 @@
+package homework.thirteenoctober;
+
+import java.util.Scanner;
+
+public class ThirteenOctoberFirstLevel {
+    public static void main(String[] args) {
+        //Вводится число. Напишите на экран сообщение "Я начинаю разбираться с циклами"
+        // количество раз, равное этому числу
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        String text = "Я начинаю разбираться с циклами";
+        while (true) {;
+            System.out.println("Я начинаю разбираться с циклами");
+        }
+    }
+}
