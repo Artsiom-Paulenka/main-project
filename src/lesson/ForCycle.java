8a686859931e1b25ffbb38a7c4709bca5ceba15e
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2022-10-23 18:09:19 +0200

    commit

---
+++ src/lesson/ForCycle.java
@@ -0,0 +1,20 @@
+package lesson;
+
+import java.util.Scanner;
+
+public class ForCycle {
+    public static void main(String[] args) {
+       //Scanner sc = new Scanner(System.in);
+       //int n = sc.nextInt();
+       //for (int i = 0; i <= n; i += 2) {
+       //    System.out.println(i);
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        for (int n = 2; n <= m; n += 2) {
+
+            System.out.println(n);
+    }
+
+    }
+}
