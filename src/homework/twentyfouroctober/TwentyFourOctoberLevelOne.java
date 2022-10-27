d27a177875739e54de3255ed6d4a1879498675e5
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2022-10-27 19:09:13 +0200

    commit

--- src/homework/twentyfouroctober/TwentyFourOctoberLevelOne.java
+++ src/homework/twentyfouroctober/TwentyFourOctoberLevelOne.java
@@ -1,5 +1,18 @@
 package homework.twentyfouroctober;
 
-public class TwentyFourOctoberLevelOne {
+import java.util.Scanner;
 
+public class TwentyFourOctoberLevelOne {
+    public static void main(String[] args) {
+        //Вводится n. Найдите сумму такого выражения: 1+1/2+1/3+1/4…..+1/n
+        Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+        double sum = 0;
+        for (int i = 1 ; i <= 1 / n; i++) {
+n = 1 / n;
+             sum = sum + n;
+            System.out.println(sum);
+        }
+
+    }
 }
