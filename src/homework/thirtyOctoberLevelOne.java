55228266b5cf2ca3e32e4d103b5822e2f466f874
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2022-11-01 13:03:25 +0100

    commit

---
+++ src/homework/thirtyOctoberLevelOne.java
@@ -0,0 +1,13 @@
+package homework;
+
+import java.util.Arrays;
+
+public class thirtyOctoberLevelOne {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int i = 1; i < 99; i++) {
+            array[i] = 10;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
--- src/lesson/ArrayClass.java
+++ src/lesson/ArrayClass.java
@@ -4,10 +4,24 @@ import java.util.Arrays;
 
 public class ArrayClass {
     public static void main(String[] args) {
-        int[] a = new int[10];
+        int[] rocketTemp = new int[10];
         for (int i = 0; i < 10; i++) {
-            a[i] = i ^ 2;
+
+
         }
-        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(rocketTemp));
     }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
