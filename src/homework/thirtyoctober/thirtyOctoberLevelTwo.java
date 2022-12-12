b3d586cd5957088bdcc2f0403ff74e2b2b5c6549
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2022-12-12 14:21:57 +0100

    commit

--- src/homework/thirtyoctober/thirtyOctoberLevelTwo.java
+++ src/homework/thirtyoctober/thirtyOctoberLevelTwo.java
@@ -6,11 +6,12 @@ public class thirtyOctoberLevelTwo {
     public static void main(String[] args) {
         int[] array = new int[100];
         int sum = 0;
-        for (int i = 0; i < 10; i++) {
+        //for (int i = 0; i < 100; i++) {
+        if (i < 10) {
             array[i] = (i * 111);
             sum = sum + array[i];
             //System.out.println(Arrays.toString(array));
         }
-               System.out.println(sum * 10);
+        System.out.println(sum * 10);
     }
 }
